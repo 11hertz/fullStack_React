db4fd82960f8e4d95503e62130862dac9cc42c88
11hertz <reachzin@naver.com>
2023-12-24 11:31:17 +0900

    React Routing: add BrowserRouter and modify structure using Routes and Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,32 @@
-import {
-  forwardRef,
-  useCallback,
-  useImperativeHandle,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
-import { MemoHello } from './components/Hello';
-import My from './components/My';
-import './App.css';
-import { useCounter } from './hooks/counter-context';
-import { useTimer } from './hooks/timer-hooks';
+import { Route, Routes } from 'react-router-dom';
 import { SessionContextProvider } from './hooks/session-context';
-import * as CN from 'classnames';
-import clsx from 'clsx';
-
-type ChildHandler = {
-  appendPeriod: () => void;
-};
-const ChildComponent = forwardRef((_, ref) => {
-  const [childText, setChildText] = useState('.');
-  const [badCount, setBadCount] = useState(0);
-  const [goodCount, setGoodCount] = useState(0);
-
-  const { useInterval, useTimeout } = useTimer();
-
-  useInterval(() => setBadCount((pre) => pre + 1), 1000);
-
-  useInterval(() => setGoodCount((pre) => pre + 1), 1000);
-
-  // useEffect(() => {
-  //   const intl = setInterval(() => setGoodCount((pre) => pre + 1), 1000);
-
-  //   return () => clearInterval(intl);
-  // }, []);
-
-  useTimeout(
-    (initSec) => {
-      setBadCount(initSec);
-      setGoodCount(initSec);
-    },
-    5000,
-    100
-  );
-
-  const handler: ChildHandler = {
-    appendPeriod: () => setChildText((c) => c + '.'),
-  };
-  useImperativeHandle(ref, () => handler);
-  return (
-    <>
-      <strong style={{ float: 'left', color: 'red' }}>{badCount}</strong>
-      childComp: {childText}
-      <strong style={{ float: 'right', color: 'green' }}>{goodCount}</strong>
-    </>
-  );
-});
+import { NotFound } from './NotFound';
+import { Home } from './components/Home';
+import './App.css';
+import { Nav } from './Nav';
+import { MemoHello } from './components/Hello';
+import { useCallback, useMemo } from 'react';
+import { useCounter } from './hooks/counter-context';
+import My from './components/My';
+import Login from './components/Login';
+import { Items } from './components/Items';
 
 function App() {
-  const { count, plusCount, minusCount } = useCounter();
-
-  const childRef = useRef<ChildHandler>(null);
-
-  // const fn = () => 'FN!';
+  const { count } = useCounter();
   const fn = useCallback(() => 'FN!', []);
   const age = useMemo(() => count + 1, [count]);
-
-  const rrr = () => ['bold', 'red'];
   return (
     <SessionContextProvider>
-      <ChildComponent ref={childRef} />
-      <hr />
-      <button onClick={() => childRef.current?.appendPeriod()}>
-        Call Child Component
-      </button>
-      <MemoHello age={age} fn={fn} />
-      <hr />
-      <My
-      // loginHandleRef={loginHandleRef}
-      />
-      {/* <div className={count % 2 === 0 ? 'card' : ''}> */}
-      <div className={CN({ card: count % 2 === 0 })}>
-        <button onClick={plusCount}>
-          count is {count > 0 ? 'Big' : 'Zero'}
-        </button>
-        <span className={clsx('card', { bold: count > 1 }, rrr())}>
-          {count}
-        </span>
-        <button onClick={minusCount}>Minus</button>
-      </div>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ttt' element={<h1>TTT</h1>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my' element={<My />} />
+        <Route path='/items' element={<Items />} />
+        <Route path='/hello' element={<MemoHello age={age} fn={fn} />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
     </SessionContextProvider>
   );
 }
